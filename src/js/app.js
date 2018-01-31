7a17babb9177956a9a91efe249f5ccf206044892
Cho Lin Tsai <max19851113@gmail.com>
2018-01-31 22:34:21 +0800

    card section api function finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,103 @@
 const css = require('../css/app.scss');
 require("font-awesome-sass-loader");
 
+function $log(x) {
+	return console.log(x);
+}
 
 function $select(x){
 	return document.querySelector(x);
 }
 
+function $selectAll(x) {
+	return document.querySelectorAll(x);
+}
+
+function $event(x, y, z, i) {
+	return x.addEventListener(y, z, i);
+}
+
+
+$event(window, 'DOMContentLoaded', windowOnLoad, false)
+
+function windowOnLoad() {
+  // get basic url and size
+	let secureUrl = `https://api.themoviedb.org/3/`;
+	let imgUrl = `https://image.tmdb.org/t/p/`;
+	let imgSize = `w185`;
+	// array 0:avengers, 1:blackpather, 2:thor3
+	// 3:doctor strange, 4:spiderman, 5:captain america
+	let movieID = [299536, 284054, 284053, 284052, 315635, 271110];
+	let selectimg = $selectAll('.card-img');
+  // make node list into array;
+	let getImgList = [...selectimg];
+	let resultList = [];
+  // make 2 arrays into a 2-dimensional array
+	for (let i = 0; i < movieID.length ; i++) {
+		resultList.push( [ movieID[i], getImgList[i] ] );
+	}
+
+  resultList.map( i => {
+			let [ x, y ] = i;
+			let xhr = new XMLHttpRequest();
+			let movieUrls = `${secureUrl}movie/${x}?api_key=${myAPI}`;
+
+			xhr.open('GET', movieUrls, true);
+
+			xhr.onload = function() {
+				if (this.status === 200) {
+					let movieOutputs = JSON.parse(this.responseText);
+					let getPath = movieOutputs.poster_path;
+					let getMovieUrls = `${imgUrl}${imgSize}${getPath}`
+					y.src = getMovieUrls;
+				}
+			}
+
+			xhr.send();
+		}
+	);
+}
 
 
 // for testing apikey
-const btnGetApi = $select('#get-btn')
-const btnSearchApi = $select('#search-btn')
+const btnGetApi = $select('#get-btn');
+const btnSearchApi = $select('#search-btn');
+const btnConfig = $select('#config-btn');
 
-btnGetApi.addEventListener('click', getApi);
-btnSearchApi.addEventListener('click', searchApi);
+$event(btnGetApi, 'click', getApi);
+$event(btnSearchApi, 'click', searchApi);
+$event(btnConfig, 'click', configApi);
 
-// avengers infinity id:299536
-// black panther id:284054
-// thor 3 id:284053
-// doctor strange id:284052
-// spiderman homecoming id:315635
-// captain america civil war id:271110
-
-function getApi() {
+function configApi() {
 	const xhr = new XMLHttpRequest();
-	const movieID = 271110
-	const myURL = `https://api.themoviedb.org/3/movie/${movieID}?api_key=${myAPI}`;
+	const secureURL = 'https://api.themoviedb.org/3/';
+	const myURL = `${secureURL}configuration?api_key=${myAPI}`;
 
 	xhr.open('GET', myURL, true);
 	xhr.onload = function() {
 		if (this.status === 200) {
 			let output = JSON.parse(this.responseText)
-			console.log(output);
+			console.log(output.images);
+		}
+	}
+
+	xhr.send()
+}
+
+function getApi() {
+	const xhr = new XMLHttpRequest();
+	const movieID = 284053;
+	const secureURL = 'https://api.themoviedb.org/3/';
+	const myURL = `${secureURL}movie/${movieID}?api_key=${myAPI}`;
+	const imgURL = `https://image.tmdb.org/t/p/`;
+	const imgSize = `w342`;
+
+	xhr.open('GET', myURL, true);
+	xhr.onload = function() {
+		if (this.status === 200) {
+			let output = JSON.parse(this.responseText)
+			let outputImgURL = `${imgURL}${imgSize}${output.poster_path}`
+			console.log(outputImgURL);
 		}
 	}
 
@@ -39,7 +105,7 @@ function getApi() {
 }
 
 
-// black panther
+
 function searchApi() {
 	const xhr = new XMLHttpRequest();
 	const secureURL = `https://api.themoviedb.org/3/`
